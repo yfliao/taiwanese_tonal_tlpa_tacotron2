5c93b20935f50907fbc92cd1188e9270f07a31b1
yfliao <yfliao0331@gmail.com>
2020-01-11 22:04:57 +0800

    add twdict.py

--- text/symbols.py
+++ text/symbols.py
@@ -5,14 +5,17 @@ Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 from text import cmudict
+from text import twdict
 
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
+_pinyin = ['@' + s for s in twdict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = _pinyin + [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+print ("symbols= ", symbols)
